64278debbcb44296d205ecc981f497c2211c88d2
VaryaKasha <varyaxx@gmail.com>
2018-05-12 12:53:26 +0300

    add task

--- lesson5/metaclass.py
+++ lesson5/metaclass.py
@@ -1,2 +1,11 @@
-class Person:
-    
+from abc import ABCMeta, abstractmethod
+
+
+class Strategy(metaclass=ABCMeta):
+    @abstractmethod
+    def select_squad(self, army):
+        pass
+
+
+st = Strategy()
+print(st)
