763139c45d31b6dc94fa45488a41768c92ac12ae
VaryaKasha <varyaxx@gmail.com>
2018-05-16 18:18:52 +0300

    add lection files

--- lesson6/logging.py
+++ lesson6/logging.py
@@ -1,6 +1,11 @@
 import logging
 
+logging.basicConfig(level=logging.INFO)
+
 log = logging.getLogger("Loggger")
+
 log.setLevel(logging.WARNING)
 
 log.setHandler(logging.StreamHandler)
+
+log.setFormatter(logging.Formatter)
