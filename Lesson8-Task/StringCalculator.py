ce7c04790397eb1eb70adec7735d15ca89549bbf
KashaVarya <varyax@yandex.ru>
2018-05-25 13:40:25 +0300

    add 2d test

--- Lesson8-Task/StringCalculator.py
+++ Lesson8-Task/StringCalculator.py
@@ -1,6 +1,10 @@
 class StringCalculator:
     def add(self, string, delimiter):
         res = 0
+
+        if string == "":
+            return 0d
+
         for num in string.split(delimiter):
             res += int(num)
 
--- Lesson8-Task/test_StringCalculator.py
+++ Lesson8-Task/test_StringCalculator.py
@@ -6,3 +6,7 @@ class StringCalculatorTestCase(unittest.TestCase):
     def test_sum(self):
         result = StringCalculator().add("12 34 20 4", " ")
         self.assertEqual(result, 70)
+
+    def test_empty(self):
+        result = StringCalculator().add("", " ")
+        self.assertEqual(result, 0)
