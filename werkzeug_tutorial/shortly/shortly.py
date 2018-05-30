35c1f8d56c85df0d2d0ad16882a12e12fc69a019
VaryaKasha <varyaxx@gmail.com>
2018-05-30 20:12:19 +0300

    add uwsgi

--- werkzeug_tutorial/shortly/shortly.py
+++ werkzeug_tutorial/shortly/shortly.py
@@ -112,4 +112,4 @@ def base36_encode(number):
 if __name__ == '__main__':
     from werkzeug.serving import run_simple
     app = create_app()
-    run_simple('127.0.0.1', 5001, app, use_debugger=True, use_reloader=True)
+    run_simple('127.0.0.1', 5000, app, use_debugger=True, use_reloader=True)
---
+++ werkzeug_tutorial/shortly/wsgi.py
@@ -0,0 +1,4 @@
+from shortly import create_app
+
+
+application = create_app()
