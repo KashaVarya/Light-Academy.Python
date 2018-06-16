274afe24cf7a639503200c22a0a1f2c5f6a616c6
VaryaKasha <varyaxx@gmail.com>
2018-06-16 13:24:23 +0300

    change view

--- django/myproj/myapp/forms.py
+++ django/myproj/myapp/forms.py
@@ -14,19 +14,19 @@ class TaskModelForm(forms.ModelForm):
         labels = {'text': 'label1_text'}
         help_text = {'text': 'help_text1'}
         error_messages = {'text': {'max_length': "error_max-length"}}
-        widgets = {  # 'checked': forms.RadioSelect(),
-                   'text': forms.Textarea()}
+        # widgets = {  # 'checked': forms.RadioSelect(),
+                  #  'text': forms.Textarea()}
 
-    # валидация
-    def clean(self):
-        text = self.data['text']
-        if text == 'start_value':
-            raise forms.ValidationError(
-                {'text': "ValidationError"}
-            )
-
-    # валидация по тексту
-    def clean_text(self):
-        text = self.cleaned_data['text']
-        if text == 'start_value_2':
-            raise forms.ValidationError('Validation error')
+    # # валидация
+    # def clean(self):
+    #     text = self.cleaned_data['text']
+    #     if text == 'start_value':
+    #         raise forms.ValidationError(
+    #             {'text': "ValidationError"}
+    #         )
+    #
+    # # валидация по тексту
+    # def clean_text(self):
+    #     text = self.cleaned_data['text']
+    #     if text == 'start_value_2':
+    #         raise forms.ValidationError('Validation error')
--- django/myproj/myapp/views.py
+++ django/myproj/myapp/views.py
@@ -54,16 +54,16 @@ class TaskView(View):
                       {'tasks': tasks, 'form': TaskModelForm(initial={'text': 'start_value'})})
 
     def post(self, request, pk=None):
-        if request.method == 'POST':
             # task = Task(text=request.POST.get('text'), checked=bool(request.POST.get('checked', False)))
-            form = TaskModelForm(request.POST)
-            if form.is_valid():
-                form.save()
-                # Task.objects.create(
-                #     text=form.cleaned_data['text'],
-                #     checked=form.cleaned_data.get('checked', False)
-                # )
-                return redirect('/tasks/')
-            return redirect('/tasks/')
+        form = TaskModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Task.objects.create(
+            #     text=form.cleaned_data['text'],
+            #     checked=form.cleaned_data.get('checked', False)
+            # )
+            return redirect('/tasks')
         # task.save()
-        return redirect('/tasks/')
+        tasks = Task.objects.all()
+        return render(request, 'myapp/index.html',
+                      {'tasks': tasks, 'form': form})
