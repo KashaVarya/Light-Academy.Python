598103c89e431535136c8ad9c9080ef11a546709
VaryaKasha <varyaxx@gmail.com>
2018-06-16 13:15:07 +0300

    change view

--- django/myproj/myapp/views.py
+++ django/myproj/myapp/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from .models import Task
 from .forms import TaskForm, TaskModelForm
 
+from django.views.generic import TemplateView, View
+
 
 # Create your views here.
 
@@ -43,3 +45,25 @@ def detail(request, pk):
         else:
             form = TaskModelForm()
     return render(request, 'myapp/detail.html', {'task': task, 'form': form})
+
+
+class TaskView(View):
+    def get(self, request, *args, **kwargs):
+        tasks = Task.objects.all()
+        return render(request, 'myapp/index.html',
+                      {'tasks': tasks, 'form': TaskModelForm(initial={'text': 'start_value'})})
+
+    def post(self, request, pk=None):
+        if request.method == 'POST':
+            # task = Task(text=request.POST.get('text'), checked=bool(request.POST.get('checked', False)))
+            form = TaskModelForm(request.POST)
+            if form.is_valid():
+                form.save()
+                # Task.objects.create(
+                #     text=form.cleaned_data['text'],
+                #     checked=form.cleaned_data.get('checked', False)
+                # )
+                return redirect('/tasks/')
+            return redirect('/tasks/')
+        # task.save()
+        return redirect('/tasks/')
--- django/myproj/myproj/urls.py
+++ django/myproj/myproj/urls.py
@@ -20,7 +20,6 @@ from myapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^tasks/$', views.index),
-    url(r'^tasks/create$', views.create_task),
+    url(r'^tasks/$', views.TaskView.as_view()),
     url(r'^tasks/(?P<pk>[0-9a-f\-]+)$', views.detail)
 ]
