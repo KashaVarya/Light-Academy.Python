5bc63db382d4e492423d03fd7b7d6c56b816438a
VaryaKasha <varyaxx@gmail.com>
2018-06-20 18:51:22 +0300

    learn View

--- django/myproj/myapp/views.py
+++ django/myproj/myapp/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render, redirect
 from .models import Task
 from .forms import TaskForm, TaskModelForm
 
-from django.views.generic import TemplateView, View
+from django.views.generic import TemplateView, View, ListView, DetailView
+from django.views.generic.edit import FormView, CreateView, UpdateView, DeleteView
+# CreateView - task/add
+# UpdateView - task/<pk>
+# DeleteView - task/<pk>/delete
+# success_url - урля, на которую перейдет после успешного выполнения
 
 
 # Create your views here.
@@ -44,6 +49,41 @@ def detail(request, pk):
         else:
             form = TaskModelForm()
     return render(request, 'myapp/detail.html', {'task': task, 'form': form})
+
+
+class TaskListView(ListView):
+    model = Task
+
+    # по умолчанию:
+    # template_name = 'tasks/task_list.html'
+    # {% for task in object.list %}
+
+    paginate_by = 5
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        data = super().get_context_data()
+        data['form'] = TaskForm()
+        return data
+
+
+# для одного объекта
+class TaskDetailView(DetailView):
+    model = Task
+
+    def get_context_data(self):
+        data = super().get_context_data()
+        data['form'] = TaskForm()
+        return data
+
+    def get_object(self, queryset=None):
+        queryset = super().get_queryset(self, request)
+
+
+class TaskFormView(FormView):
+    form_class = TaskForm
+
+    def form_valid(self, form):
+        pass
 
 
 class TaskView(View):
