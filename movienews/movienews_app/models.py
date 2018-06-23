31dd134fd5092bba23e72532093d51581e43c1df
VaryaKasha <varyaxx@gmail.com>
2018-06-23 19:52:49 +0300

    add models

--- movienews/movienews_app/models.py
+++ movienews/movienews_app/models.py
@@ -1,3 +1,60 @@
 from django.db import models
 
 # Create your models here.
+
+
+class MovieModel(models.Model):
+    id = models.AutoField()
+    title = models.CharField(max_length=64)
+    image = models.ImageField()
+    genres = models.ManyToManyField('GenreModel')
+    directors = models.ManyToManyField('DirectorModel')
+    actors = models.ManyToManyField('ActorModel')
+    date = models.DateField()
+    country = models.CharField(max_length=32)
+    duration = models.TimeField()
+    rating = models.FloatField()
+    summary = models.TextField()
+
+
+class GenreModel(models.Model):
+    id = models.AutoField()
+    title = models.CharField(max_length=64)
+    summary = models.TextField()
+    users = models.ManyToManyField('UserModel')
+
+
+class DirectorModel(models.Model):
+    id = models.AutoField()
+    last_name = models.CharField(max_length=32)
+    first_name = models.CharField(max_length=32)
+    middle_name = models.CharField(max_length=32, null=True, blank=True)
+    image = models.ImageField()
+    bio = models.TextField()
+    movies = models.ManyToManyField(MovieModel)
+
+
+class ActorModel(models.Model):
+    id = models.AutoField()
+    last_name = models.CharField(max_length=32)
+    first_name = models.CharField(max_length=32)
+    middle_name = models.CharField(max_length=32, null=True, blank=True)
+    image = models.ImageField()
+    bio = models.TextField()
+    movies = models.ManyToManyField(MovieModel)
+
+
+class ReviewModel(models.Model):
+    id = models.AutoField()
+    text = models.TextField()
+    user = models.ForeignKey('UserModel', on_delete=models.SET_NULL)
+    movie = models.ForeignKey(MovieModel, on_delete=models.SET_NULL)
+
+
+class UserModel(models.Model):
+    id = models.AutoField()
+    login = models.CharField()
+    password = models.CharField()
+    image = models.ImageField()
+    bio = models.TextField()
+    genres = models.ManyToManyField(GenreModel)
