5b74ed68a469dd0629dac695cf5be83c713cf05c
VaryaKasha <varyaxx@gmail.com>
2018-06-27 18:52:55 +0300

    add form

--- session_lection/session_app/views.py
+++ session_lection/session_app/views.py
@@ -1,32 +1,37 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
+from session_app import forms
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 
 
 def main_login(request):
-    return render(request, 'session_app/login.html')
+    form = forms.LoginForm()
+    return render(request, 'session_app/login.html', {'form': form})
 
 
 def check(request):
-    username = request.POST['username']
-    password = request.POST['password']
+    if request.method == 'POST':
+        form = forms.LoginForm(request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
 
+            user = authenticate(username=username, password=password)
 
-    user = authenticate(username=username, password=password)
-    if user is not None:
-        return render(request, 'session_app/result.html', {'mes': 'Success!'})
-    else:
-        return render(request, 'session_app/result.html', {'mes': 'Login failed!'})
+            if user is not None:
+                return render(request, 'session_app/result.html', {'mes': 'Success!'})
+            else:
+                return render(request, 'session_app/result.html', {'mes': 'Login failed!'})
 
-
-
-    #     if user.is_active:
-    #         login(request, user)
-    #     # Redirect to a success page.
-    #     else:
-    #     # Return a 'disabled account' error message
-    #     ...
+    # username = request.POST['username']
+    # password = request.POST['password']
+    #
+    # user = authenticate(username=username, password=password)
+    # if user is not None:
+    #     return render(request, 'session_app/result.html', {'mes': 'Success!'})
     # else:
-# Return an 'invalid login' error message.
+    #     return render(request, 'session_app/result.html', {'mes': 'Login failed!'})
