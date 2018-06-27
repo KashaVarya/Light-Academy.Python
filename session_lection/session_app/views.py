8e0e2d86162e6bd000c80eaf240f2d7d95cf84fb
VaryaKasha <varyaxx@gmail.com>
2018-06-27 18:22:10 +0300

    try authenticate

--- session_lection/session_app/views.py
+++ session_lection/session_app/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
 # Create your views here.
+
+
+def main_login(request):
+    return render(request, 'session_app/login.html')
+
+
+def check(request):
+    username = request.POST['username']
+    password = request.POST['password']
+
+
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        return render(request, 'session_app/result.html', {'mes': 'Success!'})
+    else:
+        return render(request, 'session_app/result.html', {'mes': 'Login failed!'})
+
+
+
+    #     if user.is_active:
+    #         login(request, user)
+    #     # Redirect to a success page.
+    #     else:
+    #     # Return a 'disabled account' error message
+    #     ...
+    # else:
+# Return an 'invalid login' error message.
--- session_lection/session_lection/urls.py
+++ session_lection/session_lection/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from session_app import views
+
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^django-admin/', admin.site.urls),
+    url(r'^$', views.main_login),
+    url(r'^login$', views.check),
 ]
