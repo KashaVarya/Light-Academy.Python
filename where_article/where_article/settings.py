f2bbd2f251d41fb8728eb3be5b3535bc7f77771b
VaryaKasha <varyaxx@gmail.com>
2018-07-04 14:04:59 +0300

    start srticle_app

--- where_article/where_article/settings.py
+++ where_article/where_article/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'where_article.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_articles',
+        'USER' : secret.user_name,
+        'PASSWORD' : secret.PASSWORD,
+        'HOST' : '127.0.0.1',
+        'PORT' : '5432',
     }
 }
 
