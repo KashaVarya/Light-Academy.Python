1f94bd1cf5a6866e8451b30eb03b6bc3966abb3b
VaryaKasha <varyaxx@gmail.com>
2018-07-04 18:27:31 +0300

    add models

---
+++ blog/app_posts/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    description = models.CharField(max_length=64)
+    is_active = models.BooleanField()
+    user = models.ForeignKey('User', null=True, on_delete=models.CASCADE)
+
+
+class Post(models.Model):
+
+    STATUS_DRAFT = 0
+    STATUS_PUBLISHED = 100
+    STATUS_REJECTED = 20
+    STATUS_TRASHED = 25
+    STATUS_AUTHORIZED = 80
+
+    STATUSES = (
+        (STATUS_DRAFT, 'Draft'),
+        (STATUS_PUBLISHED, 'Published'),
+        (STATUS_REJECTED, 'Rejected'),
+        (STATUS_TRASHED, 'Trashed'),
+        (STATUS_AUTHORIZED, 'Authorized'),
+    )
+
+    status = models.SmallIntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255, unique=True)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
